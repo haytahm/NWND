7fa6bfa3242377f775aabe0d80f0701d831cc7cb
hwq <abuaounalqunber@gmail.com>
2024-08-05 18:11:40 +0300

    last update from work pc

--- app.js
+++ app.js
@@ -9,25 +9,28 @@ app.listen(3000, () => {
 });
 
 app.get('/', (req, res) => {
+    const tasks= [{title: "Task One",
+        description: "This is the first task",
+        state : "in Progress",}];
     //res.send('<h1>Hello World</h1>');
-res.render('index');
+res.render('index' ,{title : "Home",tasks} )
 });
 app.get('/about', (req, res) => {
     //res.send('<h1>Hello World</h1>');
-res.render('about');
+res.render('about',{title : "About"} );
 });
 app.get('/tasks/newTask', (req, res) => {
     //res.send('<h1>Hello World</h1>');
-res.render('createTask');
+res.render('createTask',{title : "Adding Task"});
 });
 app.get('blogs', (req, res) => {
     //res.send('<h1>Hello World</h1>');
-res.render('blogs');
+res.render('blogs' ,{title : "All Blogs"});
 });
 app.get('/blogs/createBlog', (req, res) => {
     //res.send('<h1>Hello World</h1>');
-res.render('createBlog');
+res.render('createBlog',{title : "Creating Blog"});
 });
 
 app.use((req, res ) => {
-    res.status(404).render('page404')});
+    res.status(404).render('page404',{title : "Error 404"})});
