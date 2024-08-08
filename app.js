483a4a7dea5e46f2672ea267101ec49dc300bc0b
hwq <abuaounalqunber@gmail.com>
2024-08-08 15:06:47 +0300

    apply class 8 with the beaging of class 9 (node.js course ninja.net)

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const morgan= require ('morgan');
+
+const mongoose = require ('mongoose');
+
+
 
 const app = express();
+const dbURI = 'mongodb+srv://haythamalqunber:nzwrzdPNUL2yJU4Z@nwnd.bhaam.mongodb.net/?retryWrites=true&w=majority&appName=NWND';
+mongoose.connect(dbURI);
+
 
 app.set('view engine' , 'ejs');
  
@@ -9,12 +17,19 @@ app.listen(3000, () => {
 });
 
 app.get('/', (req, res) => {
-    const tasks= [{title: "Task One",
+    const tasks= [{title: "Abu Nour",
         description: "This is the first task",
         state : "in Progress",}];
     //res.send('<h1>Hello World</h1>');
 res.render('index' ,{title : "Home",tasks} )
 });
+/*app.use((req,res,next)=> {
+    console.log('new request made');
+        next();
+    })*/
+   app.use(express.static('public'));
+   app.use(morgan('dev'));
+   
 app.get('/about', (req, res) => {
     //res.send('<h1>Hello World</h1>');
 res.render('about',{title : "About"} );
